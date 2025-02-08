b85630f4b5b2a48b6e0b0b7e3877455b2f36822e
Haris Qureshi <92890149+HarisTechWerk@users.noreply.github.com>
2025-02-08 22:45:25 +0100

    Fixed gitignore and committed correct files

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JobTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<JobApplication> JobApplications { get; set; }
+    }
+}
---
+++ Models/JobApplication.cs
@@ -0,0 +1,11 @@
+﻿namespace JobTracker.Models
+{
+    public class JobApplication
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string JobTitle { get; set; }
+        public string Status { get; set; }
+        public DateTime AppliedDate { get; set; }
+    }
+}
